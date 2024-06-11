b5bca0e978a1cd51be1bc17611a7623a73290f04
pablorjd1 <pablo.jimenez2221@gmail.com>
2024-06-10 23:03:05 -0400

    configure theme android app

---
+++ app/src/main/java/dev/pablorjd/focustimer/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package dev.pablorjd.focustimer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryBlack = Color(0XFF000000)
+val primaryWhite = Color(0XFFFFFFFF)
+val gray = Color(0XFF888888)
+val lightGray = Color(0XFFCCCCCC)
